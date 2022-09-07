343e4e0493b367e8ee60d45771af761a56401187
Jorge Mederos <jmederosalvarado@gmail.com>
2022-09-07 08:47:32 -0400

    Fix errors in deploy token script

--- deploy-token.sh
+++ deploy-token.sh
@@ -2,12 +2,6 @@
 
 set -eux
 
-DIR=$1 # first argument should be network directory
-
-source "$DIR/spec.env"
-
-: "${KEYSTORE:=$DIR/keystore}"
-
 : "${RPC_URL:=http://localhost:8545}"
 
 : "${STAKING_CONTRACT:=0x1100000000000000000000000000000000000001}"
@@ -15,18 +9,18 @@ source "$DIR/spec.env"
 
 # get staking token address from receipt
 STAKING_TOKEN="$(forge create --json --rpc-url "$RPC_URL" \
-	--keystore "$KEYSTORE" --password "$PASSWORD" --from "$OWNER" \
+	--keystore "$OWNER_KEYSTORE" --password "$PASSWORD" \
 	--contracts "posdao-contracts/contracts" "posdao-contracts/contracts/ERC677BridgeTokenRewardable.sol:ERC677BridgeTokenRewardable" \
 	--constructor-args "STAKE" "STAKE" 18 "1001001" | jq -r '.deployedTo')"
 
 cast send --rpc-url "$RPC_URL" \
-	--keystore "$KEYSTORE" --password "$PASSWORD" --from "$OWNER" \
+	--keystore "$OWNER_KEYSTORE" --password "$PASSWORD" \
 	"$STAKING_TOKEN" "setBlockRewardContract(address)" "$BLOCK_REWARD_CONTRACT"
 
 cast send --json --rpc-url "$RPC_URL" \
-	--keystore "$KEYSTORE" --password "$PASSWORD" --from "$OWNER" \
+	--keystore "$OWNER_KEYSTORE" --password "$PASSWORD" \
 	"$STAKING_TOKEN" "setStakingContract(address)" "$STAKING_CONTRACT"
 
 cast send --rpc-url "$RPC_URL" \
-	--keystore "$KEYSTORE" --password "$PASSWORD" --from "$OWNER" \
+	--keystore "$OWNER_KEYSTORE" --password "$PASSWORD" \
 	"$STAKING_CONTRACT" "setErc677TokenContract(address)" "$STAKING_TOKEN"
